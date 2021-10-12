531ae8e3f483068c26d1687fb7b679035318e6f1
addandrei <andrei.add@yandex.ru>
2021-10-12 10:21:04 +0300

    Разбил на слои

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SiteLayoutComponent} from './shared/layouts/site-layout/site-layout.component';
+import {AuthLayoutComponent} from './shared/layouts/auth-layout/auth-layout.component';
+import {AdminLayoutComponent} from './shared/layouts/admin-layout/admin-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: SiteLayoutComponent, children: []
+  },
+  {
+    path: '', component: AuthLayoutComponent, children: []
+  },
+  {
+    path: '', component: AdminLayoutComponent, children: []
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: []
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { LoginComponent } from './login/login.component';
+import {RouterModule} from '@angular/router';
+import { MatSliderModule } from '@angular/material/slider';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { AdminLayoutComponent } from './shared/layouts/admin-layout/admin-layout.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AuthLayoutComponent,
+    SiteLayoutComponent,
+    AdminLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
